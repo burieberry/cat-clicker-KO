6ecf376be3953c0370c51f394a217dede335ae4d
Burcu Noyan <burcunoyan@gmail.com>
2017-02-05 17:45:19 -0500

    add cat nicknames and display

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ var viewModel = function() {
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
   this.imgAttr = ko.observable('https://www.flickr.com/photos.../');
 
+  // display cat level depending on clickCount
   this.level = ko.computed(function() {
       if (this.clickCount() < 10) {
         return 'Newborn';
@@ -20,6 +21,9 @@ var viewModel = function() {
         return 'Elderly';
       }
     }, this);
+
+  // display cat nicknames
+  this.nicknames = ko.observableArray(['Tabtab', 'Tiba']);
 
   // no need for get functions like before, ko does that automatically
   // need incrementCounter function to count # of clicks
