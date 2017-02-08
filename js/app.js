d61de5fc95d05375eef603f6a9520f1b7838c68c
Burcu Noyan <burcunoyan@gmail.com>
2017-02-07 22:22:10 -0500

    move current cat to initial cats list

--- js/app.js
+++ js/app.js
@@ -1,35 +1,42 @@
 var initialCats = [
   {
     clickCount: 0,
+    name:'Bib',
+    imgSrc: 'https://cdn.pixabay.com/photo/2016/11/21/14/47/adorable-1845790_960_720.jpg',
+    imgAttr: 'https://cdn.pixabay.com/photo/2016/11/21/14/47/adorable-1845790_960_720.jpg',
+    nicknames: ['Bibo', 'Bibi', 'Bibbob', 'Bilbo']
+  },
+  {
+    clickCount: 0,
     name: 'Snooze',
     imgSrc: 'https://cdn.pixabay.com/photo/2016/12/16/20/09/cat-1912251_960_720.jpg',
     imgAttr: 'https://cdn.pixabay.com/photo/2016/12/16/20/09/cat-1912251_960_720.jpg',
-    nicknames: []
+    nicknames: ['Sleepyhead']
   },
   {
     clickCount: 0,
     name: 'Madison',
     imgSrc: 'https://cdn.pixabay.com/photo/2017/01/28/18/50/cat-2016196_960_720.jpg',
     imgAttr: 'https://cdn.pixabay.com/photo/2017/01/28/18/50/cat-2016196_960_720.jpg',
-    nicknames: []
+    nicknames: ['you fancy']
   },
   {
     clickCount: 0,
     name: 'Tully',
     imgSrc: 'https://cdn.pixabay.com/photo/2017/01/19/12/05/cat-1992140_960_720.jpg',
     imgAttr: 'https://cdn.pixabay.com/photo/2017/01/19/12/05/cat-1992140_960_720.jpg',
-    nicknames: []
+    nicknames: ['']
   },
   {
     clickCount: 0,
     name: 'Poppy',
     imgSrc: 'https://pixabay.com/en/kitten-cute-cat-white-domestic-1285341/',
     imgAttr: 'https://pixabay.com/en/kitten-cute-cat-white-domestic-1285341/',
-    nicknames: []
+    nicknames: ['']
   }];
 
 var Cat = function(data) {
-  this.clickCount = ko.observable(data.clickCount); // initial clickCount is 0
+  this.clickCount = ko.observable(data.clickCount);
   this.name = ko.observable(data.name);
   this.imgSrc = ko.observable(data.imgSrc);
   this.imgAttr = ko.observable(data.imgAttr);
@@ -69,13 +76,7 @@ var viewModel = function() {
     self.catList.push(new Cat(catItem));  // self is used here
   });
 
-  this.currentCat = ko.observable(new Cat({
-    clickCount: 0,
-    name:'Bib',
-    imgSrc: 'https://cdn.pixabay.com/photo/2016/11/21/14/47/adorable-1845790_960_720.jpg',
-    imgAttr: 'https://cdn.pixabay.com/photo/2016/11/21/14/47/adorable-1845790_960_720.jpg',
-    nicknames: ['Bibo', 'Bibi', 'Bibbob', 'Bilbo']
-  }));
+  this.currentCat = ko.observable(this.catList()[0]);
 
   // count # of clicks
   this.incrementCounter = function() {
